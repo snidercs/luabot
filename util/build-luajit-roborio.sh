fc650e12a394adf2d5d20fb3e06ab45878fce489
Michael Fisher <mfisher@snidercs.org>
2024-03-31 19:28:27 -0400

    remove utilities and submods that moved to luabot-sdk

--- util/build-luajit-roborio.sh
+++
@@ -1,9 +0,0 @@
-cd luajit
-export PATH="$HOME/wpilib/2024/roborio/bin:$PATH"
-make clean
-make amalg HOST_CC="gcc -m32 -std=c99" \
-    CROSS=arm-frc2024-linux-gnueabi- \
-    XCFLAGS="-DLUAJIT_ENABLE_LUA52COMPAT=1" \
-    BUILDMODE="static" \
-    PREFIX="$HOME/SDKs/luabot/roborio"
-make install PREFIX="$HOME/SDKs/luabot/roborio"
