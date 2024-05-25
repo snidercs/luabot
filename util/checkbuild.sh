46bb5ed9a36d270f391f14b59725da95c10fa35f
Michael Fisher <mfisher31>
2024-05-25 00:48:13 -0400

    luajit.h: even better luajit.h reliability

--- util/checkbuild.sh
+++ util/checkbuild.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-set -e
+set -ex
+
+[ -z "$(which ccache)" ] || ccache -C
+
 rm -rf tbuild && \
-    meson setup tbuild &&
-    meson compile -C tbuild &&
-    meson test -C tbuild &&
+    meson setup tbuild && \
+    meson compile -C tbuild && \
+    meson test -C tbuild && \
     rm -rf tbuild
