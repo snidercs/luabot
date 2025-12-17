d87b75f77bac55f0971d774b5407253297d83abb
Michael Fisher <mfisher31@gmail.com>
2025-12-17 06:47:56 -0500

    Refactor build system: migrate to CMake, remove Meson files, and add LuaJIT support

--- src/console.c
+++ src/console.c
@@ -566,7 +566,7 @@ static int pmain(lua_State *L)
   return 0;
 }
 
-int main(int argc, char **argv)
+int console_main(int argc, char **argv)
 {
   int status;
   lua_State *L;
---
+++ src/luabot.cpp
@@ -0,0 +1,4 @@
+extern "C" int console_main(int argc, char* argv[]);
+int main(int argc, char* argv[]) {
+    return console_main (argc, argv);
+}
