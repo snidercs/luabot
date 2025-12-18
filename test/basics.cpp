dada0a763c588e6898d032d8b55ee0a4926c4148
Michael Fisher <mfisher31@gmail.com>
2025-12-18 15:02:57 -0500

    test: remove unused files

--- test/basics.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE (Basics)
-
-BOOST_AUTO_TEST_CASE (hello_world) {
-    BOOST_REQUIRE_EQUAL ("hello world", "hello world");
-}
-
-BOOST_AUTO_TEST_SUITE_END()
--- test/main.cpp
+++
@@ -1,3 +0,0 @@
-
-#define BOOST_TEST_MODULE Unit
-#include <boost/test/included/unit_test.hpp>
