a78584bd4a2c4aaf1752df7516400d3e7160b1cd
Michael Fisher <mfisher31@gmail.com>
2025-12-23 19:00:54 -0500

    feat: add init modules for wpi.cmd and wpi.frc, and update requireall to remove unnecessary entry

---
+++ bindings/wpi/cmd/init.lua
@@ -0,0 +1,9 @@
+---@module 'wpi.cmd'
+
+local M = {
+    Command = require('wpi.cmd.Command'),
+    CommandScheduler = require('wpi.cmd.CommandScheduler'),
+    FunctionalCommand = require('wpi.cmd.FunctionalCommand'),
+    Subsystem = require('wpi.cmd.Subsystem')
+}
+return M
